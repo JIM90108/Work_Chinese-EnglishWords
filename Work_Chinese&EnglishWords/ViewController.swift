50a26a027dca52f66b2d47c6e1256b95dd1a9ba6
JIM90108 <jim90108@gmail.com>
2022-04-26 12:30:27 +0800

    Initial Commit

---
+++ Work_Chinese&EnglishWords/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Work_Chinese&EnglishWords
+//
+//  Created by 彭有駿 on 2022/4/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
